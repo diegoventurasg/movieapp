31cae70b5ffb3e09dddc5dcca10727688a1d0695
Diego Ventura <diegoventurasg@gmail.com>
2022-07-29 16:28:43 -0300

    aula 4

--- lib/app/widgets/custom_list_card_widget.dart
+++ lib/app/widgets/custom_list_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/movie_details_model.dart';
+import '../pages/details_page.dart';
 import '../utils/apis_utils.dart';
 
 class CustomListCardWidget extends StatelessWidget {
@@ -12,48 +13,64 @@ class CustomListCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      decoration: BoxDecoration(
-        color: Colors.black54,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(15),
-              bottomLeft: Radius.circular(15),
+    return Material(
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => DetailsPage(movie: movie),
+              fullscreenDialog: true,
             ),
-            child: Image.network(
-              API.requestImg(movie.posterPath),
-              loadingBuilder: (_, child, progress) {
-                if (progress == null) return child;
-                return const CircularProgressIndicator.adaptive();
-              },
-            ),
+          );
+        },
+        child: Container(
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.black54,
+            borderRadius: BorderRadius.circular(15),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    movie.title,
-                    style: Theme.of(context).textTheme.headline6,
-                    softWrap: true,
-                    overflow: TextOverflow.visible,
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  bottomLeft: Radius.circular(15),
+                ),
+                child: Hero(
+                  tag: movie.id,
+                  child: Image.network(
+                    API.requestImg(movie.posterPath),
+                    loadingBuilder: (_, child, progress) {
+                      if (progress == null) return child;
+                      return const CircularProgressIndicator.adaptive();
+                    },
                   ),
-                  const Spacer(),
-                  Text('Popularidade: ${movie.popularity}'),
-                  const SizedBox(height: 10),
-                  Text('Votos: ${movie.voteAverage}'),
-                ],
+                ),
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        movie.title,
+                        style: Theme.of(context).textTheme.headline6,
+                        softWrap: true,
+                        overflow: TextOverflow.visible,
+                      ),
+                      const Spacer(),
+                      Text('Popularidade: ${movie.popularity}'),
+                      const SizedBox(height: 10),
+                      Text('Votos: ${movie.voteAverage}'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
